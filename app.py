81e16a907aebcfafa8ed83a9ac1b677574a314c8
Victor Mello <contato.devictormello@gmail.com>
2025-05-05 19:57:21 -0300

    inserindo campo de personalização de link

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, redirect, render_template
 from models import URL, Session, create_tables
 import base62
+import random
+import string
+
+#
+def generate_short_code(length=6):
+    chars = string.ascii_letters + string.digits
+    return "".join(random.choices(chars, k=length))
 
 app = Flask(__name__)
 create_tables()
@@ -8,22 +15,34 @@ create_tables()
 @app.route('/', methods=['GET', 'POST'])
 def index():
     short_url = None
+    error = None
+
     if request.method == 'POST':
         original_url = request.form['url']
+        custom_code = request.form['custom_code']
         session = Session()
 
-        new_url = URL(original_url=original_url)
-        session.add(new_url)
-        session.commit()
+        if custom_code:
+            existing = session.query(URL).filter_by(short_code=custom_code).first()
+            if existing:
+                error = "Este código já está em uso. Por favor, escolha outro."
+                session.close()
+                return render_template('index.html', short_url=None, error=error)
+            short_code = custom_code
+        else:
+            while True:
+                short_code = generate_short_code()
+                if not session.query(URL).filter_by(short_code=short_code).first():
+                    break
 
-        short_code = base62.encode(new_url.id)
-        new_url.short_code = short_code
+        new_url = URL(original_url=original_url, short_code=short_code)
+        session.add(new_url)
         session.commit()
         session.close()
 
         short_url = request.host_url + short_code
 
-    return render_template('index.html', short_url=short_url)
+    return render_template('index.html', short_url=short_url, error=error)
 
 @app.route('/<short_code>')
 def redirect_to_url(short_code):
